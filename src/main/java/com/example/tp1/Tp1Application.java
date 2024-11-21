bb64eac77f22bebe9a886925b870271c70b009c5
Nail Lamarti <nail.lamarti@etu.iut-tlse3.fr>
2024-11-21 15:54:32 +0100

    hello world

--- src/main/java/com/example/tp1/Tp1Application.java
+++ src/main/java/com/example/tp1/Tp1Application.java
@@ -8,13 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class Tp1Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Tp1Application.class, args);
     }
+
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s !", name);
     }
+
     @GetMapping("/bonjour")
     public String bonjour(@RequestParam(value = "nom", defaultValue = "tout le monde") String name) {
         return String.format("Bonjour %s !", name);
