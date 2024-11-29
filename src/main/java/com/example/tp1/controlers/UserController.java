cc0993d5c8c7b15b647526c1c9807960f6271b6b
Nail Lamarti <nail.lamarti@etu.iut-tlse3.fr>
2024-11-29 16:07:10 +0100

    new archi

---
+++ src/main/java/com/example/tp1/controlers/UserController.java
@@ -0,0 +1,26 @@
+package com.example.tp1.controlers;
+
+import com.example.tp1.models.User;
+import com.example.tp1.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping(path="/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping(path="/add")
+    public @ResponseBody String addNewUser (@RequestBody User user) {
+        userRepository.save(user);
+        return "Saved";
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
---
+++ src/main/java/com/example/tp1/models/User.java
@@ -0,0 +1,49 @@
+package com.example.tp1.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    private String nom;
+
+    private String motDePasse;
+
+    private String role;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getMotDePasse() { return motDePasse; }
+
+    public void setMotDePasse(String motDePasse) {
+        this.motDePasse = motDePasse;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
